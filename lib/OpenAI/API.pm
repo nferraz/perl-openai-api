207dbaa524f18903568e0bf81044148dc8ef98ab
Nelson Ferraz <nferraz@gmail.com>
2023-03-03 12:57:38 +0100

    Bump version

--- lib/OpenAI/API.pm
+++ lib/OpenAI/API.pm
@@ -14,7 +14,7 @@ use OpenAI::API::Request::Edit;
 use OpenAI::API::Request::Embedding;
 use OpenAI::API::Request::Moderation;
 
-our $VERSION = 0.18;
+our $VERSION = 0.19;
 
 sub new {
     my ( $class, %params ) = @_;
