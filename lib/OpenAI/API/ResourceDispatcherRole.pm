1f684c3ee2ee65e95596e48771a93a018168cbf0
Nelson Ferraz <nferraz@gmail.com>
2023-03-12 13:59:19 +0100

    Simplify dispatcher

--- lib/OpenAI/API/ResourceDispatcherRole.pm
+++ lib/OpenAI/API/ResourceDispatcherRole.pm
@@ -1,78 +1,33 @@
 package OpenAI::API::ResourceDispatcherRole;
 
-use OpenAI::API::Resource::Chat;
-use OpenAI::API::Resource::Completion;
-use OpenAI::API::Resource::Edit;
-use OpenAI::API::Resource::Embedding;
-use OpenAI::API::Resource::File::List;
-use OpenAI::API::Resource::File::Retrieve;
-use OpenAI::API::Resource::Image::Generation;
-use OpenAI::API::Resource::Model::List;
-use OpenAI::API::Resource::Model::Retrieve;
-use OpenAI::API::Resource::Moderation;
-
 use Moo::Role;
 use strictures 2;
 use namespace::clean;
 
-sub chat {
-    my ( $self, %params ) = @_;
-    my $request = OpenAI::API::Resource::Chat->new( \%params );
-    return $request->dispatch($self);
-}
+my %module_dispatcher = (
+    chat           => 'OpenAI::API::Resource::Chat',
+    completions    => 'OpenAI::API::Resource::Completion',
+    edits          => 'OpenAI::API::Resource::Edit',
+    embeddings     => 'OpenAI::API::Resource::Embedding',
+    files          => 'OpenAI::API::Resource::File::List',
+    file_retrieve  => 'OpenAI::API::Resource::File::Retrieve',
+    image_create   => 'OpenAI::API::Resource::Image::Generation',
+    models         => 'OpenAI::API::Resource::Model::List',
+    model_retrieve => 'OpenAI::API::Resource::Model::Retrieve',
+    moderations    => 'OpenAI::API::Resource::Moderation',
+);
 
-sub completions {
-    my ( $self, %params ) = @_;
-    my $request = OpenAI::API::Resource::Completion->new( \%params );
-    return $request->dispatch($self);
-}
+for my $sub_name ( keys %module_dispatcher ) {
+    my $module = $module_dispatcher{$sub_name};
 
-sub edits {
-    my ( $self, %params ) = @_;
-    my $request = OpenAI::API::Resource::Edit->new( \%params );
-    return $request->dispatch($self);
-}
+    eval "require $module";
 
-sub embeddings {
-    my ( $self, %params ) = @_;
-    my $request = OpenAI::API::Resource::Embedding->new( \%params );
-    return $request->dispatch($self);
-}
-
-sub files {
-    my ( $self, %params ) = @_;
-    my $request = OpenAI::API::Resource::File::List->new( \%params );
-    return $request->dispatch($self);
-}
-
-sub file_retrieve {
-    my ( $self, %params ) = @_;
-    my $request = OpenAI::API::Resource::File::Retrieve->new( \%params );
-    return $request->dispatch($self);
-}
-
-sub image_create {
-    my ( $self, %params ) = @_;
-    my $request = OpenAI::API::Resource::Image::Generation->new( \%params );
-    return $request->dispatch($self);
-}
-
-sub moderations {
-    my ( $self, %params ) = @_;
-    my $request = OpenAI::API::Resource::Moderation->new( \%params );
-    return $request->dispatch($self);
-}
-
-sub models {
-    my ( $self, %params ) = @_;
-    my $request = OpenAI::API::Resource::Model::List->new( \%params );
-    return $request->dispatch($self);
-}
-
-sub model_retrieve {
-    my ( $self, %params ) = @_;
-    my $request = OpenAI::API::Resource::Model::Retrieve->new( \%params );
-    return $request->dispatch($self);
+    no strict 'refs';
+    *{"OpenAI::API::ResourceDispatcherRole::$sub_name"} = sub {
+        my ( $self, %params ) = @_;
+        my $request = $module->new( \%params );
+        return $request->dispatch($self);
+    };
 }
 
 1;
